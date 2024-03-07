a3883f82f1ab62920216dea936c06a4b15bcc196
sunjx <raymooonn@gmail.com>
2024-03-07 14:19:48 +0800

    集成firebase notifee

--- App.tsx
+++ App.tsx
@@ -10,23 +10,45 @@ import { globalStorage } from './src/lib/storage'
 import KeyboardManager from 'react-native-keyboard-manager';
 import MainStack from './src/stacks/index';
 import toast from './src/lib/toast';
-import messaging from '@react-native-firebase/messaging';
+import messaging,{FirebaseMessagingTypes} from '@react-native-firebase/messaging';
+import notifee, { EventType,AndroidImportance } from '@notifee/react-native';
 // import { Notifications } from 'react-native-notifications';
-
 const requestUserPermission = async () => {
-  await messaging().requestPermission();
+
 };
 
-const onMessageReceived = async (message) => {
+const onMessageReceived = (message:any) => {
   // Notifications.postLocalNotification({
   //   title: message.notification.title,
   //   body: message.notification.body,
   //   extra: message.data,
   // });
-  toast(message.notification.title)
+  console.log('====================================');
+  console.log(message.notification);
+  console.log('====================================');
 };
 
- 
+const channelId = 'com.tdchat.default'
+async function onDisplayNotification(message: FirebaseMessagingTypes.RemoteMessage) {
+  try {
+    console.log('on background message');
+    
+    await notifee.displayNotification({
+      id: message.messageId,
+      title: message.notification?.title,
+      body: message.notification?.body,
+      android: {
+        channelId,
+        asForegroundService: true,
+        importance: AndroidImportance.HIGH,
+      },
+    });
+  } catch (error) {
+    console.error(error)
+  }
+
+}
+
 if (
   Platform.OS === 'android' &&
   UIManager.setLayoutAnimationEnabledExperimental
@@ -38,10 +60,35 @@ if (Platform.OS === 'ios') {
   KeyboardManager.setEnableAutoToolbar(false);
 }
 
-requestUserPermission()
+messaging().requestPermission().then(res=>{
+  console.log(res)
+})
 
-// messaging().onMessage(onMessageReceived);
-messaging().setBackgroundMessageHandler(onMessageReceived);
+ // Create a channel (required for Android)
+messaging().onMessage(onMessageReceived)
+
+messaging().setBackgroundMessageHandler(async(msg)=>{
+  console.log(msg) 
+  console.log('发起通知');
+  
+  await onDisplayNotification(msg)
+})
+
+// notifee.onBackgroundEvent(async ({ type, detail }) => {
+//   const { notification, pressAction } = detail;
+
+//   // Check if the user pressed the "Mark as read" action
+//   if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
+//     // Update external API
+//     await fetch(`https://my-api.com/chat/${notification.data.chatId}/read`, {
+//       method: 'POST',
+//     });
+
+//     // Remove the notification
+//     await notifee.cancelNotification(notification.id);
+//   }
+// });
+
 
 globalThis.TextEncoder = TextEncoder;
 function App(): JSX.Element {
@@ -59,9 +106,23 @@ function App(): JSX.Element {
     const token = await messaging().getToken();
     console.log('====================================');
     console.log('firebase token is : ', token);
-    toast(token)
     console.log('====================================');
+    const _channelId = await notifee.createChannel({
+      id: 'com.tdchat.default',
+      name: 'Default Channel',
+    })
 
+    // notifee.registerForegroundService(() => {
+    //   return new Promise(() => {
+    //     // Example task subscriber
+    //     onTaskUpdate(task => {
+    //       if (task.complete) {
+    //           notifee.stopForegroundService()
+    //       }
+    //     });
+    //   });
+    // });
+    
   }, []);
 
 
